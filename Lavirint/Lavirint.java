fd76c8c35d768da0159218e8b61cf068fe3b5666
Aleksa <aleksa.novkovic98@gmail.com>
2018-12-15 15:34:57 +0100

    v.1.1 - implementiran najkraci put i put sa najvise blaga + metoda za uputstvom kretanja putem kroz lavirint

--- Lavirint/Lavirint.java
+++ Lavirint/Lavirint.java
@@ -1,10 +1,11 @@
 package Lavirint;
 
+import java.util.Comparator;
 
 import org.svetovid.Svetovid;
 
 public class Lavirint {
-	private Put putanja;
+	private static Put najPut;
 	
 	private static int sirina, visina;
 	
@@ -20,6 +21,10 @@ public class Lavirint {
 			ucitaj(imeFajla);
 			stampaj();
 			System.out.println((postojiPut(0, 0)? "Posoji" : "Ne postoji") + " izlaz iz lavirinta!\n");
+//			System.out.println("Najkraci put je: \n" + najkraciPut(0, 0).stampajPut());
+			najkraciPut(0, 0).uputstvo();
+//			System.out.println("Put sa najvise blaga je: \n" + putSaNajviseBlaga(0, 0).stampajPut());
+			putSaNajviseBlaga(0, 0).uputstvo();
 		} else {
 			System.out.println("Fajl ne postoji...");
 		}
@@ -50,7 +55,10 @@ public class Lavirint {
 		if(x < 0 || x >= visina || y < 0 || y >= sirina) return false;
 		if(matrica[x][y] == ZID) return false;
 		if(bio[x][y]) return false;
-		if(matrica[x][y] == CILJ) return true;
+		if(matrica[x][y] == CILJ) {
+			bio = new boolean[visina][sirina];
+			return true;
+		}
 		
 		bio[x][y] = true;
 		
@@ -63,5 +71,62 @@ public class Lavirint {
 		return false;
 	}
 
+	private static Put najkraciPut(int x, int y) {
+		Put r = new Put();
+		najPut = null;
+		trazeniPut(x, y, new KomparatorPoDuzini(), r);
+		return najPut;
+	}
+	private static Put putSaNajviseBlaga(int x, int y) {
+		Put r = new Put();
+		najPut = null;
+		trazeniPut(x, y, new KomparatorPoVrednosti(), r);
+		return najPut;
+	}
 	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private static void trazeniPut(int x, int y, Comparator c, Put r) {
+		if(x < 0 || x >= visina || y < 0 || y >= sirina) return;
+		if(matrica[x][y] == ZID) return;
+		if(bio[x][y]) return;
+		if(matrica[x][y] == CILJ) {
+			r.dodajPolje(new Polje(x, y));
+			if(najPut == null || c.compare(najPut, r) > 0) najPut = r.copy();
+			r.obrisiPolje();
+//			bio = new boolean[visina][sirina];
+			return;
+		}
+		
+		bio[x][y] = true;
+		r.dodajPolje(new Polje(x, y, matrica[x][y]));
+		
+		trazeniPut(x+1, y, c, r);
+		trazeniPut(x-1, y, c, r);
+		trazeniPut(x, y+1, c, r);
+		trazeniPut(x, y-1, c, r);
+		
+		bio[x][y] = false;
+		r.obrisiPolje();
+	}
+	//stampa bulovu matricu kao 0 i 1... jedinice predstavljaju put do izlaza..
+	public static void stampajBio() {
+		for(int i = 0; i < visina; i++) {
+			for(int j = 0; j < sirina; j++) {
+				System.out.printf("%1$3d", (bio[i][j]? 1 : 0));
+			}
+			System.out.println();
+		}
+	}
 }
+
+class KomparatorPoDuzini implements Comparator<Put> {
+	public int compare(Put p1, Put p2) {
+		return p1.size() - p2.size();
+	}
+}
+class KomparatorPoVrednosti implements Comparator<Put> {
+	public int compare(Put p1, Put p2) {
+		return p2.vrednost() - p1.vrednost();
+	}
+}
+
--- Lavirint/Put.java
+++ Lavirint/Put.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Put {
 
-	ArrayList<Polje> put;
+	private ArrayList<Polje> put;
 
 	public Put() {
 		super();
@@ -20,12 +20,37 @@ public class Put {
 			put.remove(put.size()-1);
 		}
 	}
+	
 	public String stampajPut() {
 		String s = "___Put:___\n";
 		for(Polje polje : put) {
 			s += ("\t" + polje + "\n");
 		}
 		return s;
+	}
+
+	public int size() {
+		return put.size();
+	}
+
+	public int vrednost() {
+		int suma = 0;
+		for(Polje p : put) {
+			suma += (p.getV()>0? p.getV() : 0);
+		}
+		return suma;
+	}
+	
+	public Polje get(int index) {
+		return put.get(index);
+	}
+	
+	public Put copy() {
+		Put novi = new Put();
+		for(Polje p : put) {
+			novi.dodajPolje(p);
+		}
+		return novi;
 	}
 
 	@Override
@@ -37,4 +62,59 @@ public class Put {
 		return "Put ["+s+"]";
 	}
 	
+	//moja metoda ^_^ (izlaz u formatu: <<goTo><->>) ---> npr (dole----- ) znaci idi dole 5 puta
+	public void uputstvo() {
+		String curr = "";
+		for(int i = 0; i < put.size(); i++) {
+			if(i == 0) {
+				if(0 != put.get(i).getX()) {
+					System.out.print("dole");
+					curr = "dole";
+				}
+				if(0 != put.get(i).getY()) {
+					System.out.print("desno");
+					curr = "desno";
+				}
+			} else {
+				if(put.get(i-1).getX() != put.get(i).getX()) {
+					if(put.get(i-1).getX() < put.get(i).getX()) {
+						if(!curr.equals("dole")) {
+							System.out.print(" dole-"); 
+							curr = "dole";
+						} else System.out.print("-");
+					} else 
+						if(!curr.equals("gore")) {
+							System.out.print(" gore-"); 
+							curr = "gore";
+						} else System.out.print("-");
+				}
+				if(put.get(i-1).getY() != put.get(i).getY()) {
+					if(put.get(i-1).getY() < put.get(i).getY()) {
+						if(!curr.equals("desno")) {
+							System.out.print(" desno-"); 
+							curr = "desno";
+						} else System.out.print("-");
+					}else 
+						if(!curr.equals("levo")) {
+							System.out.print(" levo-"); 
+							curr = "levo";
+						} else System.out.print("-");
+				}
+			}
+		}
+		System.out.println();
+	}
 }
+
+/***
+*       if(r.size()>=2) {
+*			if(r.get(r.size()-2).getX() != r.get(r.size()-1).getX()) {
+*				if(r.get(r.size()-2).getX() < r.get(r.size()-1).getX()) System.out.println("dole");
+*				else System.out.println("gore");
+*			}
+*			if(r.get(r.size()-2).getY() != r.get(r.size()-1).getY()) {
+*				if(r.get(r.size()-2).getY() < r.get(r.size()-1).getY()) System.out.println("desno");
+*				else System.out.println("levo");
+*			}
+*		}
+*/
